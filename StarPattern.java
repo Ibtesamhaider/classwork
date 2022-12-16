008eabdfca7deec4c7628d4428143ad7457a1f11
Ibtesamhaider <119958962+Ibtesamhaider@users.noreply.github.com>
2022-12-16 17:51:23 +0530

    Add files via upload

--- StarPattern.java
+++ StarPattern.java
@@ -1,5 +1,27 @@
 package oops;
-
+import java.util.*;
 public class StarPattern {
-
-}
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter number of row: ");
+		int n=sc.nextInt();
+		for(int i=0;i<=n;i++)
+		{
+			for (int j=0;j<=n-i;j++)
+			{
+				System.out.print("*");
+			}
+			for(int k=1;k<=i*2-1;k++)
+			{
+				System.out.print(" ");
+			}
+			for(int l=n-i;l>=0;l--)
+			{
+				if(l!=n)
+					System.out.print("*");
+			}
+			System.out.println();
+			}
+	}
+	}
